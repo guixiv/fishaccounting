473418f9542a2afb7eb5be81d74454bc0ced772d
guixiv <1072869448@qq.com>
2018-12-21 22:18:47 +0800

    fix model

--- app/src/main/java/com/fishfishfish/fishaccounting/mvp/view/MonthAccountView.java
+++
@@ -1,7 +0,0 @@
-package com.fishfishfish.fishaccounting.mvp.view;
-
-import com.fishfishfish.fishaccounting.base.BaseView;
-import com.fishfishfish.fishaccounting.model.bean.local.MonthAccountBean;
-
-public interface MonthAccountView extends BaseView<MonthAccountBean> {
-}
--- app/src/main/java/com/fishfishfish/fishaccounting/mvp/view/MonthChartView.java
+++
@@ -1,7 +0,0 @@
-package com.fishfishfish.fishaccounting.mvp.view;
-
-import com.fishfishfish.fishaccounting.base.BaseView;
-import com.fishfishfish.fishaccounting.model.bean.local.MonthChartBean;
-
-public interface MonthChartView extends BaseView<MonthChartBean> {
-}
